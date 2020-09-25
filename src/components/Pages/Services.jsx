65fb85a761f803f986e1f75db521eba5e14604ce
Danil Konovalov <47856599+danil4905@users.noreply.github.com>
2020-09-25 12:57:40 +0400

    Add files via upload

---
+++ src/components/Pages/Services.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../App.css';
+import Footer from '../Footer/Footer';
+
+export default function Services() {
+    return (
+        <>
+    <h1 className='services'>SERVICES</h1>
+    <Footer />
+    </>)
+}
